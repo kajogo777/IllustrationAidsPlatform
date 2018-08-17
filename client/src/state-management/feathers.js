408e983f89dc0f8b87d7f49d21e0b2e67d8cc5ee
George <kajogo777@gmail.com>
2018-08-17 14:22:10 +0200

    Fix client timeout for uploads only

--- client/src/state-management/feathers.js
+++ client/src/state-management/feathers.js
@@ -10,8 +10,9 @@ const client = feathers('api/');
 // client.configure(hooks());
 client.configure(socketio(socket));
 client.configure(authentication({
-  storage: window.localStorage,
-  timeout: 120000
+  storage: window.localStorage
 }));
+
+client.service('uploads').timeout = 120000;
 
 export default client;
